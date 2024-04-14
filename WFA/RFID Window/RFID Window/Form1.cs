36f3cfb6bec7f1464e1d2f275e51f0c29f7d1516
Alexander Feuer <ajfeuer525@gmail.com>
2024-04-14 15:55:51 -0400

    added Form2

--- WFA/RFID Window/RFID Window/Form1.cs
+++ WFA/RFID Window/RFID Window/Form1.cs
@@ -11,11 +11,15 @@ namespace RFID_Window
     {
 
         public Stopwatch watch;
+        public StreamReader sr = new StreamReader(@".\AuthorizedCards.txt");
+        public string[] FileStrings = null ;
+        public string[] Names = null;
+        public string[] UIDs = null;
         public Form1()
         {
             InitializeComponent();
-            port.Open();
-            label1.Invoke(new MethodInvoker(delegate { label1.Text = "Scan Card"; }));
+            // port.Open();
+            label1.Text = "Scan Card";
         }
         private void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
@@ -23,6 +27,7 @@ namespace RFID_Window
             Debug.WriteLine(tagID);
             tagID = tagID.Substring(4,8);
             watch = Stopwatch.StartNew();
+            CreateArrays();
             if (!CheckCard(tagID)) // if UID is not in file
             {
                 label1.Invoke(new MethodInvoker(delegate { label1.Text = "ACCESS DENIED"; }));
@@ -35,21 +40,49 @@ namespace RFID_Window
             }
             
         }
-        public Boolean CheckCard(string UID)
+        public void CreateArrays()
         {
-            string path = @".\AuthorizedCards.txt";
-            StreamReader sr = new StreamReader(path);
-            string[] RegisteredUIDs = sr.ReadToEnd().Split(' ');
-            foreach (string s in RegisteredUIDs)
+            FileStrings = sr.ReadLine().Split(' ');
+            int counter = 0;
+            foreach (string s in FileStrings)
+            { 
+                if(counter%2 == 0)
+                {
+                    Names[counter/2] = s;
+                }
+                else
+                {
+                    UIDs[(counter-1)/2] = s;
+                }
+                counter++;
+            }
+        }
+
+        public Boolean CheckCard(string tagID)
+        {
+            foreach(string s in UIDs)
             {
-                Debug.WriteLine(s);
-                Debug.WriteLine(UID);
-                if (UID.Equals(s))
+                if (s.Equals(tagID))
                 {
                     return true;
                 }
             }
             return false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Control && e.KeyCode == Keys.F2) {
+                Form2 f2 = new Form2();
+                sr.Close();
+                f2.Show();
+                this.Hide();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
